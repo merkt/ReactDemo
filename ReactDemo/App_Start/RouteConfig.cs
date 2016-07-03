83b586d10cdca5fb15a23a648bb76891fc490684
Mark <markum@gmail.com>
2016-07-03 14:11:52 +0100

    Add project files.

---
+++ ReactDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ReactDemo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Comments", "api/comments", new {controller = "Home", action = "Comments"}
+                );
+
+            routes.MapRoute("NewComment", "api/comments/new", new {controller = "Home", action = "AddComment"}
+                );
+
+            routes.MapRoute("Default", "{controller}/{action}/{id}",
+                new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                );
+        }
+    }
+}
